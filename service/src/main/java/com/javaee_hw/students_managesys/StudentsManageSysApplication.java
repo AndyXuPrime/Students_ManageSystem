5a3949d9fb724e376075f1efa575f56465f3ab21
AndyXuPrime <xu_prime@andyxuprime.com>
2025-11-29 16:25:40 +0800

    init commit

---
+++ service/src/main/java/com/javaee_hw/students_managesys/StudentsManageSysApplication.java
@@ -0,0 +1,13 @@
+package com.javaee_hw.students_managesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsManageSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsManageSysApplication.class, args);
+    }
+
+}
