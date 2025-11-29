bee6c4de4ce2900610ee5baaa452a25c3133e9fa
AndyXuPrime <xu_prime@andyxuprime.com>
2025-11-29 15:43:44 +0800

    init commit

---
+++ src/test/java/com/javaee_hw/students_managesys/StudentsManageSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee_hw.students_managesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManageSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
